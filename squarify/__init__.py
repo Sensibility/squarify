e2006ac416658864acddcbc366804a34fe6c33b5
ocket8888 <ocket8888@gmail.com>
2018-04-19 10:34:42 -0600

    A bunch more work

--- squarify/__init__.py
+++ squarify/__init__.py
@@ -8,11 +8,19 @@ class Area(typing.NamedTuple):
 	"""
 	A type that represents an area, compatible with e.g. pygame screensizes, shutil termsizes etc.
 
+	The magnitude of an Area is easily found from its absolute value:
+	>>> abs(Area(16, 9))
+	144
+
+	To flip an area so that its height and width swap values, simply take the negative
+	>>> -Area(16, 9)
+	Area(width=9, height=16)
+
 	This type interacts with numeric types in the following ways:
 
-	Multiplying by integers scales the area's size
+	Multiplying by integers scales the area's size (rounds width/height to integers)
 	>>> Area(16, 9) * 2
-	Area(width=32, height=18)
+	Area(width=22, height=12)
 
 	Multiplying by a float, on the other hand, returns the scalar area of the Area scaled up by the
 	float amount
@@ -28,15 +36,15 @@ class Area(typing.NamedTuple):
 
 	True division always returns a scalar for supported argument types
 	>>> Area(32, 18) / 2
-	144.0
+	288.0
 	>>> 288.0 / Area(32, 18)
-	1.0
+	0.5
 
 	Floor division of an Area will scale it, while floor division by an Area yields a scalar
 	>>> Area(32, 18) // 2
-	Area(width=16, height=9)
+	Area(width=22, height=12)
 	>>> 288.0 // Area(16, 9)
-	2
+	2.0
 	"""
 	width: int
 	height: int
@@ -58,7 +66,8 @@ class Area(typing.NamedTuple):
 		Implements `self * other`
 		"""
 		if isinstance(other, int):
-			return type(self)(self.width * other, self.height * other)
+			toMult = other**0.5
+			return type(self)(int(self.width * toMult), self.height * toMult)
 		if isinstance(other, float):
 			return other * self.width * self.height
 		return NotImplemented
@@ -74,7 +83,7 @@ class Area(typing.NamedTuple):
 		Implements `self / other`
 		"""
 		if isinstance(other, int) or isinstance(other, float):
-			return self.width * self.height / other
+			return (self.width * self.height) / other
 		return NotImplemented
 
 	def __rtruediv__(self, other: object) -> object:
@@ -90,7 +99,8 @@ class Area(typing.NamedTuple):
 		Implements `self // other`
 		"""
 		if isinstance(other, int) or isinstance(other, float):
-			return type(self)(self.width // other, self.height // other)
+			toDiv = other**0.5
+			return type(self)(int(self.width // toDiv), int(self.height // toDiv))
 		return NotImplemented
 
 	def __rfloordiv__(self, other: object) -> object:
@@ -98,7 +108,7 @@ class Area(typing.NamedTuple):
 		Implements `other // self`
 		"""
 		if isinstance(other, int) or isinstance(other, float):
-			return self.width * self.height // other
+			return other // (self.width * self.height)
 		return NotImplemented
 
 	def __matmul__(self, other: object) -> object:
@@ -117,9 +127,52 @@ class Area(typing.NamedTuple):
 
 		return self @ other
 
+	def __abs__(self) -> int:
+		"""
+		Returns the total area encompassed
+		"""
+		return self.width * self.height
+
+	def __neg__(self) -> 'Area':
+		"""
+		Returns an Area that results from "flipping"  `self` 
+
+		i.e. one that has a width equal to `self`'s height, and a height equal to `self`'s width
+		"""
+		return type(self)(*reversed(self))
+
 class Point(typing.NamedTuple):
 	"""
 	A class that represents a point in 2D space.
+
+	Note that a Point can ONLY exist at integer (x, y) values.
+
+	Points can be added or subtracted together:
+	>>> Point(0,1) + Point(1,0)
+	Point(x=1, y=1)
+	>>> Point(1, 0) - Point(1, 0)
+	Point(x=0, y=0)
+
+	For convenience, you can also add any iterable that looks like a point in >= 2D space to a Point.
+	Note that higher dimensions are ignored when this object is the LEFT operand, but if a Point is
+	the RIGHT operand of such an operation, it will be added to the first components of the iterable
+	as though it were a higher-dimensional Point with 0 values for all dimensions above 2.
+	>>> p = Point(1, 1)
+	>>> p + (1, 0)
+	Point(x=2, y=1)
+	>>> [5, 5, 7] - Point(1,1)
+	[4, 4, 7]
+
+	Note that you should be careful with this, because the operators when a Point is on the left work
+	by casting the right operand's first two indicies to `int`s. So for example, this can be used
+	to do the following:
+	>>> Point(0,0) + "11"
+	Point(x=1, y=1)
+
+	which could maybe seem useful in some case, but note that it's impossible to add two-digit decimal
+	values to the Point in this way.
+	>>> Point(0,0) + "10, 11"
+	Point(x=1, y=0)
 	"""
 
 	x: int
@@ -135,19 +188,106 @@ class Point(typing.NamedTuple):
 		"""
 		Implements `self + other`
 		"""
-		return type(self)(self.width+other.width, self.height+other.height)
+		try:
+			toAdd = int(other[0]), int(other[1])
+			return type(self)(self.x+toAdd[0], self.y+toAdd[1])
+		except (IndexError, TypeError, ValueError):
+			# This will hit if other is not indexable,
+			# it doesn't have enough parts to seem like a point
+			# its parts are not castable to int,
+			# or its parts are not addable to our parts
+			pass
+		return NotImplemented
+
+	def __radd__(self, other):
+		"""
+		Implements `other + self`
+		"""
+		try:
+			return type(other)((other[0]+self.x, other[1]+self.y, *other[2:]))
+		except (IndexError, TypeError, ValueError):
+			pass
+		return NotImplemented
+
 
 	def __sub__(self, other):
 		"""
 		Implements `self - other`
 		"""
-		return type(self)(self.width-other.width, self.height-other.height)
+		try:
+			toSub = int(other[0]), int(other[1])
+			return type(self)(self.x-toSub[0], self.y-toSub[1])
+		except (IndexError, TypeError, ValueError):
+			# This will hit if other is not indexable,
+			# it doesn't have enough parts to seem like a point
+			# its parts are not castable to int,
+			# or its parts are not subtractable from our parts
+			pass
+		return NotImplemented
+
+	def __rsub__(self, other):
+		"""
+		Implements `other - self`
+		"""
+		try:
+			return type(other)((other[0]-self.x, other[1]-self.y, *other[2:]))
+		except (IndexError, TypeError, ValueError):
+			pass
+		return NotImplemented
+
+class Rect(typing.NamedTuple):
+	"""
+	A class that represents a rectangle.
+
+	A rectangle has a Point that marks its top-left corner, and a contained Area.
+	Rectangles are drawn in traditional computer graphics style, with the origin being the
+	top left corner of a surface. That is, the x-axis points right, and the y-axis points down.
+	So don't be surprised if the bottom-left corner of a `Rect` has a higher `y` value that its
+	top-left corner.
+	"""
+	topleft: Point
+	area: Area
+
+	@property
+	def topright(self) -> Point:
+		"""
+		Gives the rectangle's top-right corner as a Point
+		"""
+		return self.topleft + Point(self.area.width, 0)
+
+	@property
+	def bottomleft(self) -> Point:
+		"""
+		Gives the rectangles bottom-left corner as a Point
+		"""
+		return self.topleft + Point(0, self.area.height)
+
+	@property
+	def bottomright(self) -> Point:
+		"""
+		Gives the rectangle's bottom-right corner as a Point
+		"""
+		return self.topleft + Point(*self.area)
+
+	@property
+	def width(self) -> int:
+		"""
+		Gives the rectangle's width.
+		"""
+		return self.area.width
+
+	@property
+	def height(self) -> int:
+		"""
+		Gives the rectangle's height
+		"""
+		return self.area.height
 
 def normalize_sizes(sizes: typing.List[float], areaOrDX: typing.Union[Area, int], dy: int=None) -> typing.List[float]:
 	"""
 	Normalizes the `sizes` with respect to an area of size `dx`x`dy`
 	"""
-	area = Area(areaOrDX, dy) if dy else areaOrDX
+	area = Area(areaOrDX, dy) if dy else Area(*areaOrDX)
 
 	totalSize = sum(sizes)
 	return [size * area / totalSize for size in sizes]
@@ -160,7 +300,7 @@ def pad_rectangle(rect):
 		rect['y'] += 1
 		rect['dy'] -= 2
 
-def layoutrow(sizes: typing.List[float], pt: Point, area: Area) -> typing.List[typing.Tuple[Point, Area]]:
+def layoutrow(sizes: typing.List[float], pt: Point, area: Area) -> typing.List[Rect]:
 	"""
 	Lays out a row
 	"""
@@ -171,68 +311,61 @@ def layoutrow(sizes: typing.List[float], pt: Point, area: Area) -> typing.List[t
 	width = sum(sizes) / area.height
 	rects = []
 	for size in sizes:
-		rects.append((pt, Area(area.width, size / width)))
+		rects.append(Rect(pt, Area(area.width, size / width)))
 		pt = pt + Point(pt.x, pt.y + size / width)
 	return rects
 
-def layoutcol(sizes, x, y, dx, dy):
+def layoutcol(sizes: typing.List[float], pt: Point, area: Area) -> typing.List[Rect]:
 	# generate rects for each size in sizes
 	# dx < dy
 	# they will fill up width dx, and height will be determined by their area
 	# sizes should be pre-normalized wrt dx * dy (i.e., they should be same units)
-	covered_area = sum(sizes)
-	height = covered_area / dx
+	height = sum(sizes) / area.width
 	rects = []
 	for size in sizes:
-		rects.append({'x': x, 'y': y, 'dx': size / height, 'dy': height})
-		x += size / height
+		rects.append(Rect(pt, Area(size / height, area.height)))
+		pt = pt + Point(pt.x + size/height, pt.y)
 	return rects
 
-def layout(sizes, x, y, dx, dy):
-	return layoutrow(sizes, x, y, dx, dy) if dx >= dy else layoutcol(sizes, x, y, dx, dy)
+def layout(sizes: typing.List[float], pt: Point, area: Area) -> typing.List[Rect]:
+	return layoutrow(sizes, pt, area) if area.width >= area.height else layoutcol(sizes, pt, area)
 
-def leftoverrow(sizes, x, y, dx, dy):
+def leftoverrow(sizes: typing.List[float], pt: Point, area: Area) -> Rect:
 	# compute remaining area when dx >= dy
-	covered_area = sum(sizes)
-	width = covered_area / dy
-	leftover_x = x + width
-	leftover_y = y
-	leftover_dx = dx - width
-	leftover_dy = dy
-	return (leftover_x, leftover_y, leftover_dx, leftover_dy)
+	width = sum(sizes) / area.height
+	leftoverPT = pt + (width, 0)
+	leftoverArea = Area(area.width - width, area.height)
+	return Rect(leftoverPT, leftoverArea)
 
-def leftovercol(sizes, x, y, dx, dy):
+def leftovercol(sizes: typing.List[float], pt: Point, area: Area) -> Rect:
 	# compute remaining area when dx >= dy
-	covered_area = sum(sizes)
-	height = covered_area / dx
-	leftover_x = x
-	leftover_y = y + height
-	leftover_dx = dx
-	leftover_dy = dy - height
-	return (leftover_x, leftover_y, leftover_dx, leftover_dy)
+	height = sum(sizes) / area.width
+	leftoverPT = pt + (0, height)
+	leftoverArea = Area(area.width, area.height - height)
+	return Rect(leftoverPT, leftoverArea)
 
-def leftover(sizes, x, y, dx, dy):
-	return leftoverrow(sizes, x, y, dx, dy) if dx >= dy else leftovercol(sizes, x, y, dx, dy)
+def leftover(sizes: typing.List[float], pt: Point, area: Area) -> Rect:
+	return leftoverrow(sizes, pt, area) if area.width >= area.height else leftovercol(sizes, pt, area)
 
-def worst_ratio(sizes: typing.List[float], x: int, y: int, dx: int, dy: int):
-	return max([max(rect['dx'] / rect['dy'], rect['dy'] / rect['dx']) for rect in layout(sizes, x, y, dx, dy)])
+def worstRatio(sizes: typing.List[float], pt: Point, area: Area):
+	return max([max(rect.width / rect.height, rect.height / rect.width) for rect in layout(sizes, pt, area)])
 
 def squarify(sizes: typing.List[float],
              PTorX: typing.Union[Point, int],
              areaOrY: typing.Union[Area, int],
              areaOrDX: typing.Union[Area, int] = None,
-             dy: int = None) -> typing.List[typing.Tuple[float, float, float, float]]:
+             dy: int = None) -> typing.List[Rect]:
 	"""
 	Returns a list of squarified `size`s that fit in the rectangle `area`
 	with top-left corner at `pt`.
 	"""
 
 	if not areaOrDX:
-		pt = PTorX
-		area = areaOrY
+		pt = Point(*PTorX)
+		area = Area(*areaOrY)
 	elif not dy:
 		pt = Point(PTorX, areaOrY)
-		area = areaOrDX
+		area = Area(*areaOrDX)
 	else:
 		pt = Point(PTorX, areaOrY)
 		area = Area(areaOrDX, dy)
@@ -250,13 +383,17 @@ def squarify(sizes: typing.List[float],
 	
 	# figure out where 'split' should be
 	for i in range(1, len(sizes)):
-		if worst_ratio(sizes[:i], pt, area) < worst_ratio(sizes[:(i+1)], pt, area):
+		if worstRatio(sizes[:i], pt, area) < worstRatio(sizes[:(i+1)], pt, area):
 			break
 	current = sizes[:i]
 	remaining = sizes[i:]
 	
 	leftoverPT, leftoverArea = leftover(current, pt, area)
-	return layout(current, pt, area) + squarify(remaining, leftoverPT, leftoverA)
+	ret = layout(current, pt, area)
+	if ret is None:
+		raise Exception()
+	remainder = squarify(remaining, leftoverPT, leftoverArea)
+	return ret.extend(remainder) if remainder else ret
 
 def padded_squarify(sizes, x, y, dx=None, dy=None):
 	return [pad_rectangle(rect) for rect in squarify(sizes, x, y, dx, dy)]
